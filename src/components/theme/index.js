e2ff49c780076712729e8b27e05281632286601e
KattiaBodden <kattiabodden@gmail.com>
2021-04-07 01:08:55 -0600

    Editando el theme

--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -1,7 +1,11 @@
+import { DefaultTheme } from "react-native-paper";
+
 const theme = {
+  ...DefaultTheme,
     colors: {
+      ...DefaultTheme.colors,
       primary: "#3369ff",
-      secondary: "#ffda47",
+      accent: "#ffda47",
       backgroundDark: "#171c26",
       purple: "#ae3b76",
       green: "#0aebaf",
