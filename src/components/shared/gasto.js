f17a953f8bd18dabf1ee103f03d025fa957efbea
KattiaBodden <kattiabodden@gmail.com>
2021-04-12 21:56:44 -0600

    Arreglando pantalla de gastos

--- src/components/shared/gasto.js
+++ src/components/shared/gasto.js
@@ -1,6 +1,6 @@
 import React  from "react";
 import { Dimensions, StyleSheet } from "react-native";
-import { Card,Text} from "native-base";
+import { Card,Text, List, View} from "native-base";
 const { width, height } = Dimensions.get("window");
 
 
@@ -9,12 +9,11 @@ const { width, height } = Dimensions.get("window");
 const Gasto = ({ descripcion, monto }) => {
   return (
     
-      <Card style={styles.container}>
+      <List style={styles.lista}>
         <Text>{descripcion}</Text>
         <Text>{monto}</Text>
-      </Card>
-    
-    
+        <View style={styles.divisor}/>
+      </List>
   );
 };
 
@@ -26,6 +25,23 @@ const styles = StyleSheet.create({
     margin: 5,
     
   },
+
+  lista:{
+    backgroundColor:"white",
+    borderRadius: 5,
+    height: height * 0.11,
+    width: width * 0.9,
+    alignSelf:"center",
+    padding: 15,
+    
+},
+divisor:{
+  borderBottomColor: '#8a071f',
+  borderBottomWidth: 2,
+  width: width * 0.88,
+  alignSelf: "center",
+  marginTop: 3
+},
   content: {
     flex: 1,
   },
